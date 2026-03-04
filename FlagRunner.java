f75fce8838ec4f338a5b4b745645566441690c16
Chitra <chaithrachiraag2004@gmail.com>
2026-03-04 11:36:32 +0530

    instance methods

---
+++ FlagRunner.java
@@ -0,0 +1,57 @@
+class FlagRunner {
+
+    public static void main(String... args) {
+
+        Flag flag1 = new Flag();
+        flag1.hoist();
+
+        Flag flag2 = new Flag();
+        flag2.lower();
+
+        Flag flag3 = new Flag();
+        flag3.wave();
+
+        Flag flag4 = new Flag();
+        flag4.fold();
+
+        Flag flag5 = new Flag();
+        flag5.unfold();
+
+        Flag flag6 = new Flag();
+        flag6.salute();
+
+        Flag flag7 = new Flag();
+        flag7.display();
+
+        Flag flag8 = new Flag();
+        flag8.clean();
+
+        Flag flag9 = new Flag();
+        flag9.stitch();
+
+        Flag flag10 = new Flag();
+        flag10.replace();
+
+        Flag flag11 = new Flag();
+        flag11.protect();
+
+        Flag flag12 = new Flag();
+        flag12.carry();
+
+        Flag flag13 = new Flag();
+        flag13.respect();
+
+        Flag flag14 = new Flag();
+        flag14.representNation();
+
+        Flag flag15 = new Flag();
+        flag15.showColors();
+
+        // 5 Static Methods
+        Flag.nationalFlagInfo();
+        Flag.flagDay();
+        Flag.flagParts();
+        Flag.flagMaterial();
+        Flag.importance();
+    }
+}
