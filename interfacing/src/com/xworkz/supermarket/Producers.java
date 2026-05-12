34505dbaf05cc09a47dc5470594095bf9dba3b1e
Chitra <chaithrachiraag2004@gmail.com>
2026-05-12 23:49:24 +0530

    idk

---
+++ interfacing/src/com/xworkz/supermarket/Producers.java
@@ -0,0 +1,9 @@
+package com.xworkz.supermarket;
+
+public class Producers implements SuperMarket{
+
+    public Producers(SuperMarket superMarket)
+    {
+        System.out.println("===================");
+    }
+}
---
+++ interfacing/src/com/xworkz/supermarket/SuperMarket.java
@@ -0,0 +1,5 @@
+package com.xworkz.supermarket;
+
+public interface SuperMarket {
+
+}
